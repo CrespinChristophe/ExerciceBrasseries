c784a2598c8ff0c9234f3f3ca11e77efd8c691d3
Crespin <crespin.christophe@netc.eu>
2022-04-08 15:36:43 +0200

    Creation du repository

---
+++ exBrasseries/src/main/java/technifutur/crespin/exBrasseries/ExBrasseriesApplication.java
@@ -0,0 +1,13 @@
+package technifutur.crespin.exBrasseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBrasseriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBrasseriesApplication.class, args);
+	}
+
+}
---
+++ exBrasseries/src/test/java/technifutur/crespin/exBrasseries/ExBrasseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package technifutur.crespin.exBrasseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBrasseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
